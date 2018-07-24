e920277f100e9c79e63d5a6e4e3d9181017e2534
ziyou8 <scaitong@gmail.com>
2018-07-24 17:50:35 +0800

    refactor moving

--- vbal/alien_invasion/alien_invasion.py
+++ vbal/alien_invasion/alien_invasion.py
@@ -12,7 +12,7 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while True:
         gf.check_events(ship)
--- vbal/alien_invasion/game_functions.py
+++ vbal/alien_invasion/game_functions.py
@@ -3,23 +3,27 @@ import sys
 import pygame
 #from ship import Ship
 
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left True
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_color)
