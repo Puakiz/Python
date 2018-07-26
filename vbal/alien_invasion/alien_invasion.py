519fa2a64f2f551c24b5f4ac209d2f4899db9cfa
vbal <348522794@qq.com>
2018-07-26 17:52:04 +0800

    Alien is coming

--- vbal/alien_invasion/alien_invasion.py
+++ vbal/alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -16,12 +17,14 @@ def run_game():
     ship = Ship(ai_settings, screen)
     bullets = Group()
 
+    # Make an alien
+    alien = Alien(ai_settings, screen)
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
         # print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
--- vbal/alien_invasion/game_functions.py
+++ vbal/alien_invasion/game_functions.py
@@ -31,11 +31,12 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
 
     pygame.display.flip()
 
