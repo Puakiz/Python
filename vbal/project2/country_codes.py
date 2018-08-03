37f925f431af376652c06bec835fd29337223d6e
vbal <348522794@qq.com>
2018-08-03 05:41:00 -0400

    Grouping Countries by Population

--- vbal/project2/country_codes.py
+++ vbal/project2/country_codes.py
@@ -8,8 +8,8 @@ def get_country_code(country_name):
     # If the country wasn't found, return None.
     return None
 
-
+""" testing
 print(get_country_code('Andorra'))
 print(get_country_code('United Arab Emirates'))
 print(get_country_code('Afghanistan'))
-
+"""
--- vbal/project2/world_population.py
+++ vbal/project2/world_population.py
@@ -1,14 +1,47 @@
 import json
 
+import pygal
+from pygal.style import RotateStyle
+
+from country_codes import get_country_code
+
 # Load the data into a list.
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Print the 2010 population for each country.
+# Build a dictionary of population data.
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
-        country_name = pop_dict['Country Name']
+        country = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        print(country_name + ": " + str(population))
+        code = get_country_code(country)
+        if code:
+            cc_populations[code] = population
+
+# Group the countries into 3 population levels.
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+# See how many countries are in each level.
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm_style = RotateStyle('#336699')
+wm = pygal.maps.world.World(style=wm_style)
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+
+wm.render_to_file('World_population.svg')
+
+
+
 
