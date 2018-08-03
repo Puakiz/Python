8a1f5d69b35e49917911bcf18168f9ec5ffa7479
vbal <348522794@qq.com>
2018-08-02 22:36:41 -0400

    Plotting Dates

--- vbal/project2/highs_lows.py
+++ vbal/project2/highs_lows.py
@@ -1,4 +1,7 @@
 import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
 
 # Get high temperatures from file.
 filename = 'sitka_weather_07-2014.csv'
@@ -6,8 +9,29 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
-        highs.append(row[1])
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
 
-    print(highs)
+        high = int(row[1])
+        highs.append(high)
+
+    # print(highs)
+
+# Plot data.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red')
+
+# Format plot.
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+
+# draw the date labels diagonally to prevent them from overlapping.
+fig.autofmt_xdate()
+
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
