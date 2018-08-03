9ca21c273f7e45f1e295b28cadfa9e5afe76a09f
vbal <348522794@qq.com>
2018-08-02 22:18:11 -0400

    Extracting and Reading Data

--- vbal/project2/highs_lows.py
+++ vbal/project2/highs_lows.py
@@ -1,9 +1,13 @@
 import csv
 
+# Get high temperatures from file.
+filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    for row in reader:
+        highs.append(row[1])
 
+    print(highs)
