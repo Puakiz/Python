0372d573d01354d33d5899dbdbaee1891a7381f9
vbal <348522794@qq.com>
2018-08-02 22:55:19 -0400

    Shading an Area in the Chart

--- vbal/project2/highs_lows.py
+++ vbal/project2/highs_lows.py
@@ -4,12 +4,12 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # Get high temperatures from file.
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -17,14 +17,17 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
-    # print(highs)
+        low = int(row[3])
+        lows.append(low)
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 
 # draw the date labels diagonally to prevent them from overlapping.
