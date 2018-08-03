1c1c6a430c03e6f7f2ad8312bd07c8c627d5fdae
vbal <348522794@qq.com>
2018-08-02 22:10:36 -0400

    Printing the headers and their positions

--- vbal/project2/highs_lows.py
+++ vbal/project2/highs_lows.py
@@ -1,7 +1,9 @@
 import csv
 
-filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
