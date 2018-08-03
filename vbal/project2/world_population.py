5817422b4ae1d3ce7371f2f58d321519fbb9a8e7
vbal <348522794@qq.com>
2018-08-02 23:16:05 -0400

    Extracting Relevant Data

---
+++ vbal/project2/world_population.py
@@ -0,0 +1,14 @@
+import json
+
+# Load the data into a list.
+filename = 'population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)
+
+# Print the 2010 population for each country.
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = pop_dict['Value']
+        print(country_name + ": " + population)
+
