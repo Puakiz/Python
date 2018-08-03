a83f4f6842da52c55d285fedd3e6134e4a127f3a
vbal <348522794@qq.com>
2018-08-02 23:33:12 -0400

    No i18n module, using pygal.maps.world instead.

--- vbal/project2/world_population.py
+++ vbal/project2/world_population.py
@@ -9,6 +9,6 @@ with open(filename) as f:
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
-        population = pop_dict['Value']
-        print(country_name + ": " + population)
+        population = int(float(pop_dict['Value']))
+        print(country_name + ": " + str(population))
 
