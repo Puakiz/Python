11c3b0b036fe1f0eb12d548367efcb30729e7c28
vbal <348522794@qq.com>
2018-08-13 03:36:54 -0400

    visualizing Repositories Using Pygal

--- vbal/api/python_repos.py
+++ vbal/api/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -11,22 +13,17 @@ print("Total repositories:", response_dict['total_count'])
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-# Examine the first repository.
-# repo_dict = repo_dicts[0]
-print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('Name:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    # print('Created:', repo_dict['created_at'])
-    # print('Updated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
-'''
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
-'''
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
