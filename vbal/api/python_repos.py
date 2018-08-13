227fbdb9706112f475dfcbd828c751187081e8ee
vbal <348522794@qq.com>
2018-08-13 04:10:18 -0400

    Plotting the Data, please note some project's description is None type, so you need to convert the returned value to str type to prevent the error.

--- vbal/api/python_repos.py
+++ vbal/api/python_repos.py
@@ -14,10 +14,15 @@ print("Total repositories:", response_dict['total_count'])
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+    }
+    plot_dicts.append(plot_dict)
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
@@ -35,5 +40,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
