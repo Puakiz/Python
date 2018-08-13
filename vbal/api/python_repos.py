e34ee81b455ff1c0826ad9f362782c382bb08bb2
vbal <348522794@qq.com>
2018-08-13 03:26:31 -0400

    Summarizing the Top Repositories

--- vbal/api/python_repos.py
+++ vbal/api/python_repos.py
@@ -14,7 +14,19 @@ repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
 # Examine the first repository.
-repo_dict = repo_dicts[0]
+# repo_dict = repo_dicts[0]
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    # print('Created:', repo_dict['created_at'])
+    # print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
+
+'''
 print("\nKeys:", len(repo_dict))
 for key in sorted(repo_dict.keys()):
     print(key)
+'''
