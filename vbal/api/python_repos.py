fd4f5ea286e1f813bce7877fa819ba61fcc4bb4f
vbal <348522794@qq.com>
2018-08-13 04:14:30 -0400

    Adding Clickable Links to Our Graph

--- vbal/api/python_repos.py
+++ vbal/api/python_repos.py
@@ -21,6 +21,7 @@ for repo_dict in repo_dicts:
     plot_dict = {
         'value': repo_dict['stargazers_count'],
         'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url'],
     }
     plot_dicts.append(plot_dict)
 
