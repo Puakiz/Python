933ccfea2e212b36ff342a6152fc2578dd8d09e3
vbal <348522794@qq.com>
2018-08-13 03:48:56 -0400

    Adding Custom Tooltips

--- vbal/api/python_repos.py
+++ vbal/api/python_repos.py
@@ -21,7 +21,17 @@ for repo_dict in repo_dicts:
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
