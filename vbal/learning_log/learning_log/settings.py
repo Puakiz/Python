14201c6d4cc91417cd5b1a4af6fd21c6723b3524
vbal <348522794@qq.com>
2018-08-16 04:48:34 -0400

    Defining the Main Part of the Page

--- vbal/learning_log/learning_log/settings.py
+++ vbal/learning_log/learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party apps
+    'bootstrap3',
+
     # My apps
     'learning_logs',
     'users',
@@ -125,3 +128,8 @@ STATIC_URL = '/static/'
 
 # My settings
 LOGIN_URL = '/users/login/'
+
+# Settings for django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
